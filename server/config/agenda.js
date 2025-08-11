a10e87d54308bd5558ee0667dad042fd1a44c3be
afsal-techon <techon.afsal@gmail.com>
2025-08-11 17:37:28 +0530

    done

--- server/config/agenda.js
+++ server/config/agenda.js
@@ -1,6 +1,9 @@
 import Agenda from "agenda";
 import mongoose from "mongoose";
 import KOT_NOTIFICATION from '../model/kotNotification.js'
+import { getIO } from '../config/socket.js'
+
+
 
 const agenda = new Agenda({ db: { address: process.env.MONGO_URI_CLOUD || 'mongodb://127.0.0.1:27017/Restaurant-pos' } });
 
@@ -13,6 +16,11 @@ agenda.define("mark kot as ready", async (job) => {
     kot.preparedAt = new Date();
     await kot.save();
     console.log(`KOT ${kotId} marked as Ready`);
+
+
+       const io = getIO();
+      io.to(`kitchen:${kot.kitchenId}`).emit('kot_status_update', kot);
+
   }
 });
 
@@ -23,6 +31,10 @@ agenda.define("reject kot after 24 hours", async (job) => {
     kot.status = 'Rejected';
     await kot.save();
     console.log(`KOT ${kotId} marked as Rejected (after 24h)`);
+
+       const io = getIO();
+      io.to(`kitchen:${kot.kitchenId}`).emit('kot_status_update', kot);
+    
   }
 });
 
