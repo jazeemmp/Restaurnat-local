f7181ff4e5c3b10b1a83b4763aa75e6762e40e83
afsal-techon <techon.afsal@gmail.com>
2025-08-18 14:32:26 +0530

    done ok

--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -360,7 +360,6 @@ router.get('/vat/pdf',VerifyToken,checkOfflinePermission('Reports'),generateVATR
 router.get('/vat/excel',VerifyToken,checkOfflinePermission('Reports'),generateVATReportExcel);
 
 
-
 //partner
 router.post('/partner',VerifyToken,checkOfflinePermission('Admin'),addPartner)
 router.put('/partner',VerifyToken,checkOfflinePermission('Admin'),updatePartner)
